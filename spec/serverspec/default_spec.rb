7df233c3b56b09f5060e6a3a29eb1cf5744b0d7c
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-05-10 16:45:44 +0900

    support zone and master-slave configuration

--- spec/serverspec/default_spec.rb
+++ spec/serverspec/default_spec.rb
@@ -19,6 +19,8 @@ when 'freebsd'
   db_dir = '/var/db/nsd'
 end
 
+key_file = "#{ config_dir }/my_tsig_key.key"
+
 describe package(package) do
   it { should be_installed }
 end 
@@ -37,7 +39,6 @@ describe file(config) do
   its(:content) { should match Regexp.escape("pidfile: \"#{run_dir}/nsd.pid\"") }
   its(:content) { should match Regexp.escape("xfrdfile: \"#{db_dir}/xfrd.state\"") }
   its(:content) { should match /verbosity: 0/ }
-  its(:content) { should match /statistics: 3600/ }
   its(:content) { should match /round-robin: no/ }
 end
 
@@ -61,4 +62,28 @@ ports.each do |p|
   describe port(p) do
     it { should be_listening }
   end
+end
+
+describe file(key_file) do
+  it { should be_file }
+  it { should be_mode 600 }
+  its(:content) { should match Regexp.escape("secret: Qes2X7V8Fjg+EMlqng1qlCvErGFxXWa4Gxfy1uDWKvQ=") }
+  its(:content) { should match /algorithm: hmac-sha256/ }
+end
+
+describe command('drill -o rd example.com @127.0.0.1 ns') do
+  its(:stdout) { should match /;; flags: qr aa ; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1/ }
+  its(:stdout) { should match /example\.com.\s+86400\s+IN\s+NS\s+ns1\.example\.com\./ }
+  its(:stdout) { should match /ns1\.example\.com\.\s+120\s+IN\s+A\s+192\.168\.0\.1/ }
+  its(:stdout) { should match /;; SERVER: 127\.0\.0\.1/ }
+  its(:stderr) { should match /^$/ }
+end
+
+describe command('drill example.com @127.0.0.1 axfr') do
+  its(:stdout) { should match /example\.com\.\s+86400\s+IN\s+SOA\s+ns1\.example\.com\.\s+hostmaster\.example\.com\.\s+2013020201\s+10800\s+3600\s+604800\s+3600/ }
+  its(:stderr) { should match /^$/ }
+end
+
+describe command('drill -y my_tsig_key:Qes2X7V8Fjg+EMlqng1qlCvErGFxXWa4Gxfy1uDWKvQ=:hmac-sha256 example.com @192.168.133.101 axfr') do
+  its(:stderr) { should match /AXFR failed/ }
 end
--- spec/serverspec/remote_control_spec.rb
+++ spec/serverspec/remote_control_spec.rb
@@ -19,6 +19,8 @@ when 'freebsd'
   db_dir = '/var/db/nsd'
 end
 
+key_file = "#{ config_dir }/my_tsig_key.key"
+
 describe package(package) do
   it { should be_installed }
 end
@@ -74,7 +76,35 @@ case os[:family]
 when 'freebsd'
 else
   describe port(8952) do
-    it { should_not be_listening.on('192.168.133.i01').with('tcp') }
-    it { should be_listening.on('192.168.133.i01').with('tcp') }
+    it { should_not be_listening.on('192.168.133.101').with('tcp') }
+    it { should be_listening.on('192.168.133.101').with('tcp') }
   end
+end
+
+describe file(key_file) do
+  it { should be_file }
+  it { should be_mode 600 }
+  its(:content) { should match Regexp.escape("secret: Qes2X7V8Fjg+EMlqng1qlCvErGFxXWa4Gxfy1uDWKvQ=") }
+  its(:content) { should match /algorithm: hmac-sha256/ }
+end
+
+describe command('drill -o rd example.com @192.168.133.100  ns') do
+  its(:stdout) { should match /;; flags: qr aa ; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1/ }
+  its(:stdout) { should match /example\.com.\s+86400\s+IN\s+NS\s+ns1\.example\.com\./ }
+  its(:stdout) { should match /ns1\.example\.com\.\s+120\s+IN\s+A\s+192\.168\.0\.1/ }
+  its(:stdout) { should match /;; SERVER: 192\.168\.133\.100/ }
+  its(:stderr) { should match /^$/ }
+end
+
+describe command('drill -o rd example.com @127.0.0.1 ns') do
+  its(:stdout) { should match /;; flags: qr aa ; QUERY: 1, ANSWER: 1, AUTHORITY: 0, ADDITIONAL: 1/ }
+  its(:stdout) { should match /example\.com.\s+86400\s+IN\s+NS\s+ns1\.example\.com\./ }
+  its(:stdout) { should match /ns1\.example\.com\.\s+120\s+IN\s+A\s+192\.168\.0\.1/ }
+  its(:stdout) { should match /;; SERVER: 127\.0\.0\.1/ }
+  its(:stderr) { should match /^$/ }
+end
+
+describe command('drill -y my_tsig_key:Qes2X7V8Fjg+EMlqng1qlCvErGFxXWa4Gxfy1uDWKvQ=:hmac-sha256 example.com @192.168.133.100 axfr') do
+  its(:stdout) { should match /example\.com\.\s+86400\s+IN\s+SOA\s+ns1\.example\.com\.\s+hostmaster\.example\.com\.\s+2013020201\s+10800\s+3600\s+604800\s+3600/ }
+  its(:stderr) { should match /^$/ }
 end
