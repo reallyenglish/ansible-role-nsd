43e2ac9539193fc3bdd50fb866b59d030742f004
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-10-10 16:05:55 +0900

    another test in tests/serverspec/default_spec.rb

--- tests/integration/example/spec/default_spec.rb
+++ tests/integration/example/spec/default_spec.rb
@@ -5,6 +5,21 @@ end
 
 context 'after provisioning finished' do
 
+  describe server(:ns1) do
+
+    it "should NOT be able to AXFR from ns2" do
+      result = current_server.ssh_exec('drill -y my_tsig_key:Qes2X7V8Fjg+EMlqng1qlCvErGFxXWa4Gxfy1uDWKvQ=:hmac-sha256 example.com @192.168.133.101 axfr')
+      expect(result).to match(/AXFR failed/)
+    end
+
+    #describe command('drill example.com @127.0.0.1 axfr') do
+    it "should be able to AXFR from localhost" do
+      result = current_server.ssh_exec('drill example.com @127.0.0.1 axfr')
+      expect(result).to match(/example\.com\.\s+86400\s+IN\s+SOA\s+ns1\.example\.com\.\s+hostmaster\.example\.com\.\s+2013020201\s+10800\s+3600\s+604800\s+3600/)
+    end
+
+  end
+
   describe server(:ns2) do
 
     it "should be able to query NS example.com and get a reply from 192.168.133.100" do
